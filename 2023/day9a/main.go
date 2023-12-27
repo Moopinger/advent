7175c4a999c217914842107c7d4683e012d3dd71
mc air <mcair@Temwanis-MacBook-Air.local>
2023-12-27 14:57:07 +0200

    day9a

---
+++ 2023/day9a/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getNextRow(input_row []int) []int {
+	var nextRow []int
+
+	for i := 1; i < len(input_row); i += 1 {
+		difference := input_row[i] - input_row[i-1]
+		nextRow = append(nextRow, difference)
+	}
+
+	return nextRow
+
+}
+
+func allZero(row []int) bool {
+	for _, val := range row {
+		if val != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func predictNext(rows [][]int) int {
+
+	height := len(rows) - 1
+
+	//appends last zero
+	rows[height] = append(rows[height], 0)
+
+	for i := (height - 1); i >= 0; i = i - 1 {
+		rows[i] = append(rows[i], rows[i][len(rows[i])-1]+rows[i+1][len(rows[i+1])-1])
+	}
+
+	return rows[0][len(rows[0])-1]
+
+}
+
+func main() {
+
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//remove trailing line
+	data = data[:len(data)-1]
+	challenge_inputs := strings.Split(string(data), "\n")
+
+	next_answers := []int{}
+
+	for _, challenge_input := range challenge_inputs {
+
+		steps := []int{}
+		rows := [][]int{}
+
+		inputs := strings.Split(challenge_input, " ")
+
+		for _, input := range inputs {
+			int_val, _ := strconv.Atoi(input)
+			steps = append(steps, int_val)
+		}
+
+		rows = append(rows, steps)
+
+		for true {
+
+			row := rows[len(rows)-1]
+			next_row := getNextRow(row)
+			rows = append(rows, next_row)
+
+			if allZero(next_row) == true {
+				break
+			}
+		}
+
+		answer := predictNext(rows)
+		fmt.Printf("The answer is... %d\n", answer)
+		next_answers = append(next_answers, answer)
+	}
+	total := 0
+	for _, val := range next_answers {
+		total += val
+	}
+	fmt.Printf("[+]Total values are %d\n", total)
+}
